0887ef744e20e6c8a2519ba30089ed5f25fac47a
Leah Sandacz <sandas@outlook.com>
2018-04-06 22:36:48 -0700

    Update welcome page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ class IndexPage extends React.Component {
     return (
       <div>
         <Bio>
-          <h2>Web and Mobile Development ahoy!</h2>
-          <p>I'm passionate about modern web and mobile techonologies such as React, React Native, Node, and Express.</p>
+          <h2>Web Development ahoy!</h2>
+          <p>I'm passionate about developing applications for the modern web. I'm experienced with web technologies such as React, Node.js, Express, Apollo Client, and GraphQL.</p>
           <p>Mobile game development is a side interest, with my engine of choice being Unity.</p>
         </Bio>
         <SourceLinkBlock/>
