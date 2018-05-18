ce1a5d140a61d0ed48403dc145627a2e24f2ce7c
Leah Sandacz <sandas@outlook.com>
2018-05-18 16:03:30 -0700

    Front page description

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ class IndexPage extends React.Component {
       <div>
         <Bio>
           <h2>Web Development ahoy!</h2>
-          <p>I'm passionate about developing applications for the modern web. I'm experienced with web technologies such as React, Node.js, Express, Apollo Client, and GraphQL.</p>
+          <p>I'm passionate about developing applications for the modern web. I'm experienced with web technologies such as React, Node.js, Express, MongoDB, GraphQL and Apollo Client.</p>
           <p>Mobile game development is a side interest, with my engine of choice being Unity.</p>
         </Bio>
         <SourceLinkBlock/>
